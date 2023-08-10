e7d2a271c7e78342f1723f578438e19a065309bd
Marcin <marcinwisniewski707@gmail.com>
2023-08-10 21:05:19 +0200

    loading animation fixes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Suspense fallback={(
-      <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%'}}>
+      <Box sx={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
         <CircularProgress/>
       </Box>)}>
       <App />
