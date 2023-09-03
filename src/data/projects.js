7bc9c1516953507fe7a4ac20d458139c5867af1b
Marcin <marcinwisniewski707@gmail.com>
2023-09-03 18:15:07 +0200

    Addicional changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,6 +3,7 @@ import image from '../images/chessrocket.png';
 import image1 from '../images/currency.png'
 import image3 from '../images/project3.png'
 import image4 from '../images/Movies.png'
+import image5 from '../images/Zrzut ekranu 2023-09-03 114852.png'
 
 const data = [
   {
@@ -28,6 +29,12 @@ const data = [
     title: 'Shopping list web application',
     img: image3,
     link: 'https://eclectic-profiterole-e5c7a6.netlify.app/'
+  },
+    {
+    key: '5',
+    title: 'Digital Business Card',
+    img: image5,
+    link: 'https://stellular-pixie-318ab1.netlify.app/'
   },
 
 ];
