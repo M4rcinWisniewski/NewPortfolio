a93b9f503c0de46b26ccbc4dfd3542378347043c
Marcin <marcinwisniewski707@gmail.com>
2023-09-06 22:52:51 +0200

    checkout

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -3,7 +3,7 @@ import Landing from '../components/Landing';
 import Project from '../components/project';
 import data from '../data/projects';
 import Services from '../components/services';
-import { Typography, Box, Stack } from '@mui/material';
+import { Typography, Box, Stack} from '@mui/material';
 import { useTranslation } from "react-i18next";
   import useEmblaCarousel from 'embla-carousel-react'
   import Autoplay from 'embla-carousel-autoplay'
@@ -39,12 +39,21 @@ const Home = ({ theme, isValue}) => {
         <Landing theme={theme}/>
         <Box sx={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column', marginBottom:'20vh'}}>
         <Typography sx={{fontSize:'2.5rem', fontWeight:'600'}}>{t('Projects.1')}</Typography>
-        {window.innerWidth <= 977 && 
+        {window.innerWidth <= 977 ?
           <Box ref={emblaRef} sx={{display: 'flex', justifyContent:'center', alignItems:'center', flexWrap:'wrap', gap: '10vw', marginTop:'10vh'}}>
             <Stack direction="row"  FlexWrap="wrap"  style={{ width: '60vw', margin: 0, display: 'flex', justifyContent: 'left',  flex: '0 0 50%', gap:'5vw'}}>
               {projects }
             </Stack>
-          </Box>}
+          </Box>
+          
+            :
+        
+                  <Box sx={{display: 'flex', justifyContent:'center', alignItems:'center', flexWrap:'wrap', gap: '10vw', marginTop:'10vh'}}>
+                  
+                    {projects }
+                </Box>
+        }
+
 
 
         </Box>
