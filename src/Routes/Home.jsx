23d023963559c7fa114f183f9dbd1a7a924cd567
Marcin <marcinwisniewski707@gmail.com>
2023-09-10 11:13:46 +0200

    another fixes

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -3,23 +3,17 @@ import Landing from '../components/Landing';
 import Project from '../components/project';
 import data from '../data/projects';
 import Services from '../components/services';
-import { Typography, Box, Stack} from '@mui/material';
+import { Typography, Box} from '@mui/material';
 import { useTranslation } from "react-i18next";
-  import useEmblaCarousel from 'embla-carousel-react'
-  import Autoplay from 'embla-carousel-autoplay'
+
   
 const Home = ({ theme, isValue}) => {
   
 
   
   const {t} = useTranslation();
-  const autoplayOptions = {
-    delay: 5000,
-    jump: false,
-    align: 'center',
-    
-  }
-  const [emblaRef] = useEmblaCarousel({loop:false},  [Autoplay(autoplayOptions)])
+
+
     const projects = data.map(item => {
         return(
           <Project 
@@ -39,20 +33,13 @@ const Home = ({ theme, isValue}) => {
         <Landing theme={theme}/>
         <Box sx={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column', marginBottom:'20vh'}}>
         <Typography sx={{fontSize:'2.5rem', fontWeight:'600'}}>{t('Projects.1')}</Typography>
-        {window.innerWidth <= 977 ?
-          <Box ref={emblaRef} sx={{display: 'flex', justifyContent:'center', alignItems:'center', flexWrap:'wrap', gap: '10vw', marginTop:'10vh'}}>
-            <Stack direction="row"  FlexWrap="wrap"  style={{ width: '60vw', margin: 0, display: 'flex', justifyContent: 'left',  flex: '0 0 50%', gap:'5vw'}}>
-              {projects }
-            </Stack>
-          </Box>
-          
-            :
+
         
                   <Box sx={{display: 'flex', justifyContent:'center', alignItems:'center', flexWrap:'wrap', gap: '10vw', marginTop:'10vh'}}>
                   
                     {projects }
                 </Box>
-        }
+
 
 
 
